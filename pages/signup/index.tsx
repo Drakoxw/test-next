b841d602bd1cbd1af9d9e165d5f1bd797c3bc6ec
Drako <desarrollo3@aveonline.co>
2022-11-14 11:16:04 -0500

    feat export app

---
+++ pages/signup/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Signup() {
+  return (
+    <>
+      <br />
+      <br />
+      <h1>Hola : Signup</h1>
+      <Link href='/'>
+        Home
+      </Link>
+    </>
+  );
+}
