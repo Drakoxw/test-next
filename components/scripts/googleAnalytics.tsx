811e3191e3e3772b991693471d1dedf0759810d9
Drako <desarrollo3@aveonline.co>
2022-11-21 20:26:08 -0500

    feat: google analytics

--- components/scripts/googleAnalytics.tsx
+++ components/scripts/googleAnalytics.tsx
@@ -4,20 +4,20 @@ import { GOOGLE_ANALYTIC_ID } from '@constants/index'
 import { logClear, logDev } from 'utils'
 
 export default function GoogleAnalytics () {
+  const srcUrl = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTIC_ID}`
   return (
   <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+    <Script strategy="afterInteractive" src={srcUrl}/>
     <Script
       id='google-analytics'
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GOOGLE_ANALYTIC_ID}', {
-            page_path: window.location.pathname,
-          });
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${GOOGLE_ANALYTIC_ID}');
         `,
         }}
     />
