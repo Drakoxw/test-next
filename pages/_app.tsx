771dddc7d34644d43a77e1dd2a513ce8023b0a81
Drako <desarrollo3@aveonline.co>
2022-11-23 02:22:44 -0500

    feat: login ave 3.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
-import { useRouter } from 'next/router'
 import { gtmEventsHandler } from '../components/scripts/tagManager'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
 
   useEffect(() => {
 
     const mainDataLayer = {
       event: pageProps.event || null,
       pageTypeName: pageProps.page || null,
-      url: router.pathname,
     };
 
     gtmEventsHandler(mainDataLayer);
-    
+
   }, [pageProps])
   return <Component {...pageProps} />
 }
